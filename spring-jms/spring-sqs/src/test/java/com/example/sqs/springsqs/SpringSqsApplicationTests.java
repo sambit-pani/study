e58fe34d395b6dd49cd14316a864bb94de5dbd26
Sambit Pani <sambitpani16@gmail.com>
2019-01-27 12:06:35 +0100

    addedsqs project

---
+++ spring-jms/spring-sqs/src/test/java/com/example/sqs/springsqs/SpringSqsApplicationTests.java
@@ -0,0 +1,17 @@
+package com.example.sqs.springsqs;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringSqsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
--- spring-test/spring-test/src/main/resources/data.sql
+++
@@ -1,3 +0,0 @@
-insert into item(id,name,price,quantity) values(1,'grapes',144,12);
-insert into item(id,name,price,quantity) values(2,'orange',11,124);
-insert into item(id,name,price,quantity) values(3,'apple',55,87);
