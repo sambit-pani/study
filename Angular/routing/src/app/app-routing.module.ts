b0d6e11285702574e4522b175c93e768a71e12ed
Sambit Pani <sambitpani16@gmail.com>
2019-03-13 23:50:19 +0100

    added child routing

--- Angular/routing/src/app/app-routing.module.ts
+++ Angular/routing/src/app/app-routing.module.ts
@@ -2,10 +2,22 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { SettingComponent } from './setting/setting.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { SettingContactComponent } from './setting-contact/setting-contact.component';
+import { SettingProfileComponent } from './setting-profile/setting-profile.component';
 
 const routes: Routes = [
+  {path:'',redirectTo:'/home',pathMatch:'full'},
   {path:'home',component: HomeComponent },
-  {path:'settings',component:SettingComponent}
+  {
+    path:'settings',
+    component:SettingComponent,
+    children: [
+      {path:'contact',component:SettingContactComponent},
+      {path:'profile',component:SettingProfileComponent}
+    ]
+  },
+  {path:'**',component:PageNotFoundComponent}
 ];
 
 @NgModule({
--- Angular/routing/src/app/app.module.ts
+++ Angular/routing/src/app/app.module.ts
@@ -5,12 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { SettingComponent } from './setting/setting.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { SettingContactComponent } from './setting-contact/setting-contact.component';
+import { SettingProfileComponent } from './setting-profile/setting-profile.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    SettingComponent
+    SettingComponent,
+    PageNotFoundComponent,
+    SettingContactComponent,
+    SettingProfileComponent
   ],
   imports: [
     BrowserModule,
